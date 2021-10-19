6e51531f42a52d5b40711b7e3b10dfbd4bdcfd0b
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-10-19 12:33:20 +0200

    added example of how to call rust from c

--- calling_rust/src/lib.rs
+++ calling_rust/src/lib.rs
@@ -1,8 +1,15 @@
+
+#[no_mangle]
+pub extern "C" fn addition(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
+        let result = addition(2, 2);
         assert_eq!(result, 4);
     }
 }
