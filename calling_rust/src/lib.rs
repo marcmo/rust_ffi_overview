7e395210e23601455e5c65074f4230d77e009385
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-10-19 13:18:30 +0200

    adding rust greeting

--- calling_rust/src/lib.rs
+++ calling_rust/src/lib.rs
@@ -1,7 +1,14 @@
+use ferris_says::say;
+use std::io::{ stdout, BufWriter };
 
 #[no_mangle]
 pub extern "C" fn addition(a: u32, b: u32) -> u32 {
-    println!("RUST adding {} and {}", a, b);
+    let greeting = format!("RUST adding {} and {}", a, b);
+    // let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout());
+    say(greeting.as_bytes(), width, &mut writer).unwrap();
     a + b
 }
 
